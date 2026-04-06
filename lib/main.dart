93ca8dae2456e916aac54beef22fcb4c4e77b538
Eimy <eimyalejandragomez05@gmail.com>
2026-04-06 15:50:04 -0500

    Eimy version

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/inicio-eimy.dart';
+
+void main() {
+  runApp(const MiApp());
+}
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
